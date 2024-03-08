654310d61c0af49dfdca3b45a6e630c672267a89
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-03-08 16:20:12 +0100

    Symbol table

--- src/parser/ast_visitor/symboltablevisitor.py
+++ src/parser/ast_visitor/symboltablevisitor.py
@@ -1,37 +1,90 @@
-from typing import Any
-
 from .visitor import Visitor
-from ..ast import ast as AST, expression as EXPR
+
+from ..ast.program import Program
+from ..ast import expression as EXPR
+from ..ast import ast as AST
+from ..ast.main_function import MainFunction
+from ..ast.compound_statement import CompoundStatement
+from ..ast.expression import CastExpression
+from ..ast.type import Type
+from ..ast.declaration import Declaration
+from ..ast.variable import Variable
+
+from ..symbol_table.symboltable import SymbolTable, Symbol
+
 
 
 class SymbolTableVisitor(Visitor):
     def __init__(self):
-        self.current_scope = None  # Keep track of the current scope's symbol table
+        self.current_scope = SymbolTable(scope_name="global", scope_level=0)
 
-    def visit_program(self, ast: AST.AST):
-        # Set up the global scope symbol table
-        self.current_scope = ast.symbol_table
+    def enter_scope(self, scope_name):
+        self.current_scope = self.current_scope.enter_new_scope(scope_name)
+
+    def exit_scope(self):
+        if self.current_scope.parent is not None:
+            self.current_scope = self.current_scope.parent
+
+    def visit_program(self, program: Program) -> Any:
+        for statement in program.statements:
+            self.visit(statement)
+
+    def visit_main_function(self, main_function: MainFunction) -> Any:
+        self.enter_scope("main_function") # Main Function scope
         # Visit children nodes
-        for statement in ast.statements:
-            self.visit_ast(statement)
+        for statement in main_function.body.statements:
+            self.visit(statement)
+        self.exit_scope()
+
+    def visit_compound_statement(self, compound_statement: CompoundStatement) -> Any:
+        self.enter_scope("compound") # Compound statements (e.g., blocks) introduce a new scope
+        # Visit children nodes
+        for statement in compound_statement.statements:
+            self.visit(statement)
+        self.exit_scope() # Exit the scope
+
+    def visit_declaration(self, declaration: Declaration) -> Any:
+        for variable in declaration.variables:
+            self.visit(variable)
+
+    def visit_type(self, type_node: Type) -> Any:
+        pass
+
+    def visit_variable(self, variable: Variable) -> Any:
+        if self.current_scope.lookup(variable.identifier, current_scope_only=True):
+            raise Exception(f"Redeclaration of variable: {variable.identifier}")
+
+        else:
+            symbol = Symbol(name=variable.identifier, type=variable.type, scope_level=self.current_scope.scope_level)
+            self.current_scope.insert(variable.identifier, symbol)
+
+    def visit_cast_expression(self, cast_expression: CastExpression) -> Any:
+        self.visit(cast_expression.expr)
 
     def visit_binary_arithmetic(self, expr: EXPR.BinaryArithmetic) -> Any:
-        pass
+        self.visit(expr.left)
+        self.visit(expr.right)
 
     def visit_binary_bitwise_arithmetic(self, expr: EXPR.BinaryBitwiseArithmetic) -> Any:
-        pass
+        self.visit(expr.left)
+        self.visit(expr.right)
 
     def visit_binary_logical_operation(self, expr: EXPR.BinaryLogicalOperation) -> Any:
-        pass
+        self.visit(expr.left)
+        self.visit(expr.right)
 
     def visit_comparison_operation(self, expr: EXPR.ComparisonOperation) -> Any:
-        pass
+        self.visit(expr.left)
+        self.visit(expr.right)
 
     def visit_unary_expression(self, expr: EXPR.UnaryExpression) -> Any:
-        pass
+        self.visit(expr.expr)
 
     def visit_shift_expression(self, expr: EXPR.ShiftExpression) -> Any:
-        pass
+        self.visit(expr.left)
+        self.visit(expr.right)
 
     def visit_int(self, expr: EXPR.INT) -> Any:
         pass
+
+
--- src/parser/symbol_table/symboltable.py
+++ src/parser/symbol_table/symboltable.py
@@ -1,15 +1,42 @@
+class Symbol:
+    def __init__(self, name, type=None, scope_level=None, line=None, additional_info=None):
+        self.name = name
+        self.type = type
+        self.scope_level = scope_level
+        self.line = line  # Useful for error reporting
+        self.additional_info = additional_info  # For any additional symbol-related information
+
+    def __str__(self):
+        return f"Symbol(name={self.name}, type={self.type}, scope_level={self.scope_level}, line={self.line}, additional_info={self.additional_info})"
+
 class SymbolTable:
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, scope_name=None, scope_level=0):
         self.symbols = {}
         self.parent = parent
+        self.scope_name = scope_name  # Name of the scope (useful for debugging)
+        self.scope_level = scope_level  # Nested scope level
 
-    def insert(self, symbol):
-        self.symbols[symbol.name] = symbol
+    def insert(self, name, symbol):
+        self.symbols[name] = symbol
 
     def lookup(self, name, current_scope_only=False):
         symbol = self.symbols.get(name)
         if symbol is not None:
             return symbol
-        if not current_scope_only and self.parent is not None:
+        if not current_scope_only and self.parent:
             return self.parent.lookup(name)
         return None
+
+    def enter_new_scope(self, scope_name):
+        return SymbolTable(parent=self, scope_name=scope_name, scope_level=self.scope_level + 1)
+
+    def __str__(self):
+        return f"SymbolTable(scope_name={self.scope_name}, scope_level={self.scope_level}, symbols={list(self.symbols.keys())})"
+
+""""
+Example usage:
+    global_table = SymbolTable(scope_name="global")
+    function_table = global_table.enter_new_scope("function")
+source: https://www.geeksforgeeks.org/symbol-table-compiler/
+"""
+
