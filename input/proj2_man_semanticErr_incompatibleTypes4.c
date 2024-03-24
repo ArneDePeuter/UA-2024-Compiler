78e3e3e75e03e3b0d5e25c05d55c1030cdc28791
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-03-24 18:17:45 +0100

    Expected errors

--- input/proj2_man_semanticErr_incompatibleTypes4.c
+++ input/proj2_man_semanticErr_incompatibleTypes4.c
@@ -4,7 +4,7 @@ int main() {
     float* ptr2 = 0;
 
     // wrong types
-    float* ptr3 = ptr2 + ptr;
-    ptr3 = ptr - ptr3;
+    float* ptr3 = ptr2 + ptr; // error: invalid operands to binary expression ('float *' and 'int *')
+    ptr3 = ptr - ptr3; // error: 'int *' and 'float *' are not pointers to compatible types
 
 }
