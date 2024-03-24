8bd36a49ac71940563c35899630166447ce13a5d
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-03-24 19:18:42 +0100

    Expected Warning

--- input/proj2_opt_pass_constPointerToNonConstPointer1.c
+++ input/proj2_opt_pass_constPointerToNonConstPointer1.c
@@ -1,7 +1,7 @@
 int main() {
 const char c = 'a';
 const char* chr_ptr = &c;
-char* non_const_ptr = chr_ptr;
+char* non_const_ptr = chr_ptr; // warning: initializing 'char *' with an expression of type 'const char *' discards qualifiers [-Wincompatible-pointer-types-discards-qualifiers]
 *non_const_ptr = 'c';
 
 }
