bce30846aed86cdabcde4ce8286243c9602a5023
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-03-25 21:13:53 +0100

    NULL Pointers

--- tests/test_semantic_errors.py
+++ tests/test_semantic_errors.py
@@ -1,6 +1,7 @@
 import os
 
-from compiler.frontend import tree_from_file
+from compiler.frontend import tree_from_file, tree_to_ast
+from compiler.frontend.symbol_table.symbol_table_visitor import SymbolTableVisitor, SymbolTable
 from compiler.core.errors.semantic_error import SemanticError
 
 error_dict = {
@@ -14,7 +15,9 @@ error_dict = {
     "proj2_man_semanticErr_redeclaration.c": SemanticError("Variable 'x' is already declared.", 5, 0),
     "proj2_man_semanticErr_redefinition.c": SemanticError("Variable 'f' is already declared.", 4, 0), # TODO: Mabye trow a more specific error message
     # TODO: Implement proj2_man_semanticErr_rvalueAssignment1.c
+    "proj2_man_semanticErr_rvalueAssignment1.c": None,
     # TODO: Implement proj2_man_semanticErr_rvalueAssignment2.c
+    "proj2_man_semanticErr_rvalueAssignment2.c": None,
     "proj2_man_semanticErr_undeclaredVariable1.c": SemanticError("Variable 'some_variable' must have an initializer. Meaning it is undeclared.", 4, 0),
     "proj2_man_semanticErr_undeclaredVariable2.c": SemanticError("Use of undeclared variable 'x'.", 3, 0),
     "proj2_man_semanticErr_undeclaredVariable3.c": SemanticError("Use of undeclared identifier 'z'.", 2, 12)
@@ -26,16 +29,20 @@ def test_semantic_err() -> None:
 
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-
-        if filename in error_dict:
-            continue
-
         print(f"Testing {filename}")
 
-        try:
-            tree_from_file(f"{directory.decode()}/{filename}")
-        except Exception as e:
-            error_dict[filename] = str(e)
+        if error_dict[filename] is None:
+            print(f"{filename} to be implemented")
         else:
-            print(f"{filename} passed")
-            assert False
+            try:
+                tree = tree_from_file(f"{directory.decode()}/{filename}")
+                ast = tree_to_ast(tree)
+
+                symbol_table_visitor = SymbolTableVisitor(symbol_table=SymbolTable())
+                symbol_table_visitor.visit_program(ast)
+            except Exception as e:
+                error_dict[filename] = str(e)
+                print(f"{filename} passed")
+                assert True
+            else:
+                assert False
