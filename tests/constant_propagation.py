519760484bb45a15af217551c3c50a70001b58e0
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2024-03-28 19:34:18 +0100

    FIX: constant propagation

---
+++ tests/constant_propagation.py
@@ -0,0 +1,61 @@
+from compiler.core import ast
+from compiler.frontend import tree_from_str, tree_to_ast
+from compiler.middleend import ConstantPropagationVisitor
+
+
+def test_1():
+    test_str = """
+    int main() {
+        int a = 5;
+        int b = a + 3;
+    }
+    """
+
+    tree, input_stream = tree_from_str(test_str)
+    output_ast = tree_to_ast(tree, input_stream)
+    result = ConstantPropagationVisitor().visit(output_ast)
+
+    assert isinstance(result, ast.Program)
+    assert type(result.statements[0].body.statements[1].qualifiers[0].initializer.left) == ast.INT
+    assert result.statements[0].body.statements[1].qualifiers[0].initializer.left.value == 5
+
+
+def test_2():
+    test_str = """
+    int main() {
+        int a = 5;
+        int b = 3;
+        int c = a + b;
+    }
+    """
+
+    tree, input_stream = tree_from_str(test_str)
+    output_ast = tree_to_ast(tree, input_stream)
+    result = ConstantPropagationVisitor().visit(output_ast)
+
+    assert isinstance(result, ast.Program)
+    assert type(result.statements[0].body.statements[2].qualifiers[0].initializer.left) == ast.IDENTIFIER
+    assert type(result.statements[0].body.statements[2].qualifiers[0].initializer.right) == ast.INT
+    assert result.statements[0].body.statements[2].qualifiers[0].initializer.right.value == 3
+
+
+def test_3():
+    test_str = """
+    int main() {
+        const int a = 5;
+        int b = 3;
+        int c = a + b;
+    }
+    """
+
+    tree, input_stream = tree_from_str(test_str)
+    output_ast = tree_to_ast(tree, input_stream)
+    result = ConstantPropagationVisitor().visit(output_ast)
+
+    assert isinstance(result, ast.Program)
+    assert type(result.statements[0].body.statements[2].qualifiers[0].initializer.left) == ast.INT
+    assert result.statements[0].body.statements[2].qualifiers[0].initializer.left.value == 5
+    assert type(result.statements[0].body.statements[2].qualifiers[0].initializer.right) == ast.INT
+    assert result.statements[0].body.statements[2].qualifiers[0].initializer.right.value == 3
+
+
