201fe5305bd9baff280aa3e838d86cead0573be1
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2024-04-20 02:21:50 +0200

    FEAT: test dot visitors for no crashes

--- tests/pass/test_pass.py
+++ tests/pass/test_pass.py
@@ -5,6 +5,8 @@ from compiler.frontend import tree_from_file, tree_to_ast
 from compiler.frontend.symbol_table.symbol_table_visitor import SymbolTableVisitor, SymbolTable
 from compiler.middleend import optimise_ast
 from compiler.backend.llvm_target.llvm_ir_generator import LLVMIRGenerator
+from compiler.utils.symboltabledotvisitor import SymbolTableDotVisitor
+from compiler.utils.astdotvisitor import AstDotVisitor
 
 
 @pytest.mark.parametrize("input_file", os.listdir("./tests/pass/files"))
@@ -19,3 +21,9 @@ def test_pass(input_file) -> None:
 
     llvm_ir_generator = LLVMIRGenerator()
     llvm_ir = llvm_ir_generator.generate_llvm_ir(ast)
+
+    symbol_table_dot_visitor = SymbolTableDotVisitor()
+    symbol_table_dot_visitor.generate_dot(symbol_table_visitor.symbol_table.global_scope)
+
+    ast_dot_visitor = AstDotVisitor()
+    ast_dot_visitor.visit(ast)
