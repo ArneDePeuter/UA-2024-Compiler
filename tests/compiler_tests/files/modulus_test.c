e9ea6186de611e5155f4cd43cea34ea2d4063b7b
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-04-20 11:24:42 +0200

    Modulus

---
+++ tests/compiler_tests/files/modulus_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char c = 10; // Initialize char with a numeric value
+    int divisor = 3;
+    char result = c % divisor; // Modulus operation
+    printf("%d", result);
+}
