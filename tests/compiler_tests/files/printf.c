66c58e979ca48397c9a21bcab25d7bd28a0175cd
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-04-20 15:11:45 +0200

    Printf test

---
+++ tests/compiler_tests/files/printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // String
+    //const char *str = "Hello, World!";
+    //printf("%s", str);
+
+    // Decimal integer
+    int dec = 42;
+    printf("%d", dec);
+
+    // Hexadecimal integer
+    int hex = 255;
+    printf("%x", hex);
+
+    // Floating-point number
+    float flt = 3.14159;
+    printf("%f", flt);
+
+    // Character
+    char chr = 'A';
+    printf("%c", chr);
+
+}
