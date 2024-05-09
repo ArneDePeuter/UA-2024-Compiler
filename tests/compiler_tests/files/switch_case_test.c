82de57c5837bbf2d458d385c6a46110c34d146a9
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-09 16:16:32 +0300

    FIX: switch_case_test.c

--- tests/compiler_tests/files/switch_case_test.c
+++ tests/compiler_tests/files/switch_case_test.c
@@ -4,25 +4,22 @@ int main() {
     char grade = 'B';
     switch (grade) {
         case 'A':
-            //printf("Excellent!\n");
-            printf("%c", grade);
+            printf("Excellent!\n");
             break;
         case 'B':
+            printf("Well done\n");
+            break;
         case 'C':
-            //printf("Well done\n");
-            printf("%c", grade);
+            printf("You passed\n");
             break;
         case 'D':
-            //printf("You passed\n");
-            printf("%c", grade);
+            printf("Better try again\n");
             break;
         case 'F':
-            //printf("Better try again\n");
-            printf("%c", grade);
+            printf("Better try again\n");
             break;
         default:
-            //printf("Invalid grade\n");
-            printf("%c", grade);
+            printf("Invalid grade\n");
     }
     return 0;
 }
