b6176a18db7bc8dc4c1e4559cbf694fa8e864507
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-09 16:13:45 +0300

    FIX: array_pointer_test.c

--- tests/compiler_tests/files/array_pointer_test.c
+++ tests/compiler_tests/files/array_pointer_test.c
@@ -3,9 +3,7 @@
 int main() {
     int numbers[5] = {1, 2, 3, 4, 5};
     int *ptr = numbers;  // Pointer to the first element
-    //printf("First element: %d\n", *ptr);
-    printf("%d", *ptr);
-    //printf("Third element: %d\n", *(ptr + 2));
-    printf("%d", *(ptr + 2));
+    printf("First element: %d\n", ptr[0]);
+    printf("Third element: %d\n", ptr[2]);
     return 0;
 }
