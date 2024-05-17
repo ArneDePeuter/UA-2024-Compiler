4b316495712d5ec106811b3ca19ab2040623c042
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-17 15:40:04 +0200

    FEAT: registermanager

--- mipslite/module.py
+++ mipslite/module.py
@@ -1,9 +1,11 @@
 from .block import Block
 from .function import Function
+from .register_manager import RegisterManager
 
 class Module:
     def __init__(self):
         self.blocks: list[Block] = []
+        self.register_manager = RegisterManager()
 
     def __repr__(self):
         return "\n".join(map(str, self.blocks))
