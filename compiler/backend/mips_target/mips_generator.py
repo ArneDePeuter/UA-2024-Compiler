0b28d8a057e19d345a7b17cdeb03990663a8eaad
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-20 15:46:52 +0200

    FIX: shift expr. amount & value not left & right attributes

--- compiler/backend/mips_target/mips_generator.py
+++ compiler/backend/mips_target/mips_generator.py
@@ -196,17 +196,19 @@ class MIPSGenerator(AstVisitor):
         return result_reg
 
     def visit_shift_expression(self, node: ast.ShiftExpression):
-        left = self.visit_expression(node.left)
-        right = self.visit_expression(node.right)
+        value = self.visit_expression(node.amount)
+        amount = self.visit_expression(node.value)
+
         result_reg = self.module.register_manager.allocate('temp')
 
         if node.operator == ast.ShiftExpression.Operator.LEFT:
-            self.builder.add_instruction(f"sll {result_reg}, {left}, {right}")
+            self.builder.add_instruction(f"sll {result_reg}, {value}, {amount}")
         elif node.operator == ast.ShiftExpression.Operator.RIGHT:
-            self.builder.add_instruction(f"srl {result_reg}, {left}, {right}")
+            self.builder.add_instruction(f"srl {result_reg}, {value}, {amount}")
 
-        self.module.register_manager.free(left)
-        self.module.register_manager.free(right)
+        self.module.register_manager.free(value)
+        self.module.register_manager.free(amount)
+
         return result_reg
 
     def visit_function_call(self, node: ast.FunctionCall):
