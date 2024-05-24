b08e8e17e8f8c7b42d139a88d3b8e44834b71077
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-24 22:00:46 +0200

    FIX: test cases

--- tests/compiler_tests/files/explicit_type_casting.c
+++ tests/compiler_tests/files/explicit_type_casting.c
@@ -21,15 +21,5 @@ int main() {
     int i4 = (int)c2;
     printf("Char %c cast to integer: %d\n", c2, i4);
 
-    // Double to float
-    double d = 9.87654321;
-    float f3 = (float)d;
-    printf("Double %f cast to float: %f\n", d, f3);
-
-    // Float to double
-    float f4 = 1.2345f;
-    double d2 = (double)f4;
-    printf("Float %f cast to double: %f\n", f4, d2);
-
     return 0;
 }
