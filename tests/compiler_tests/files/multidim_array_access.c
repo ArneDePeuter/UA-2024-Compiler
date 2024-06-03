6ba650f20be7d35d1de84b931e76977311bb8bf9
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-06-03 13:36:47 +0200

    FIX: test case

--- tests/compiler_tests/files/multidim_array_access.c
+++ tests/compiler_tests/files/multidim_array_access.c
@@ -4,7 +4,7 @@ int main() {
     int f[10][10];
     f[0][0] = 1;
     printf("%d\n", f[0][0]);
-    printf("%d\n", f[0][1]);
-    printf("%d\n", f[3+3][5+3]);
+    // printf("%d\n", f[0][1]); undefined behavior
+    // printf("%d\n", f[3+3][5+3]); undefined behavior
     return 0;
 }
